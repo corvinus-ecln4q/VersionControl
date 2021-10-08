4d419c89648e28a698ed637ff81c72946f326d6d
MatyusLevente <levente.matyus@stud.uni-corvinus.hu>
2021-10-08 14:14:39 +0200

    Load Data

---
+++ ExcelExport/ExcelExport/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelExport
+{
+    public partial class Form1 : Form
+    {
+        RealEstateEntities1 context = new RealEstateEntities1();
+        List<Flat> Flats;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = Flats;
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+    }
+}
