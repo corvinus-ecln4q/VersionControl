a520c3b9ff6fa16c16c974749c20bb25d0a3ef1b
Levente Mátyus <levente.matyus@stud.uni-corvinus.hu>
2021-11-12 16:47:20 +0100

    Vége

---
+++ FejlesztésiMinták/FejlesztésiMinták/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace FejlesztésiMinták
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.Button();
+            this.Car = new System.Windows.Forms.Button();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.AutoSize = true;
+            this.mainPanel.Controls.Add(this.button1);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.Ball);
+            this.mainPanel.Controls.Add(this.Car);
+            this.mainPanel.Location = new System.Drawing.Point(-2, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(522, 449);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next";
+            // 
+            // Ball
+            // 
+            this.Ball.Location = new System.Drawing.Point(135, 95);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(79, 51);
+            this.Ball.TabIndex = 1;
+            this.Ball.Text = "Ball";
+            this.Ball.UseVisualStyleBackColor = true;
+            this.Ball.Click += new System.EventHandler(this.Ball_Click);
+            // 
+            // Car
+            // 
+            this.Car.Location = new System.Drawing.Point(40, 95);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(79, 51);
+            this.Car.TabIndex = 0;
+            this.Car.Text = "Car";
+            this.Car.UseVisualStyleBackColor = true;
+            this.Car.Click += new System.EventHandler(this.Car_Click);
+            // 
+            // createTimer
+            // 
+            this.createTimer.Enabled = true;
+            this.createTimer.Interval = 3000;
+            this.createTimer.Tick += new System.EventHandler(this.createTimer_Tick);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 10;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Location = new System.Drawing.Point(220, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 51);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 448);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Ball;
+        private System.Windows.Forms.Button Car;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
